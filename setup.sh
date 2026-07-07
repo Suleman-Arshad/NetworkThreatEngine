816a057c1f5293e159e2c22076ec1b48b1a1d3e6
Suleman-Arshad <sulemanarshad0682@gmail.com>
2026-07-07 13:56:47 +0500

    adding the sudo apt install libsqlite3-dev sqlite3 command in setup.sh file

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 echo "Installing dependencies for Network Threat Engine..."
 sudo apt update
 sudo apt install build-essential cmake libpcap-dev -y
+sudo apt install libsqlite3-dev sqlite3
 echo "All dependencies installed successfully!"
