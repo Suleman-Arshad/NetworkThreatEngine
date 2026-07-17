fb35b28bc3c1d028e75562360890e60a3a076d44
Suleman-Arshad <sulemanarshad0682@gmail.com>
2026-07-17 15:10:57 +0500

    add the command to install ncurses

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ echo "Installing dependencies for Network Threat Engine..."
 sudo apt update
 sudo apt install build-essential cmake libpcap-dev -y
 sudo apt install libsqlite3-dev sqlite3
+sudo apt-get install libncurses-dev
 echo "All dependencies installed successfully!"
